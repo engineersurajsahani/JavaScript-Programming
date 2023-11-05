f33a8109af066929d61f1721e500701ec1d2b14d
radhashyam9321@gmail.com <radhashyam9321@gmail.com>
2023-11-05 18:23:36 +0530

    05/11/2023

---
+++ function_program.js
@@ -0,0 +1,23 @@
+// function without parameter and without return type 
+
+function hello(){
+    print("Hello");
+}
+
+// function with parameter and without return type 
+
+function hello(a){
+    print(a);
+}
+
+// function without parameter and with return type 
+
+function hello(){
+    return "Hello";
+}
+
+// function with parameter and with return type 
+
+function hello(a){
+    return a;
+}
---
+++ object_program.js
@@ -0,0 +1,19 @@
+
+// 1) JavaScript Object by object literal
+
+emp1={
+    id:1,
+    name:"Suraj Sahani",
+    salary:50000
+}
+
+print(emp1.id+" "+emp1.name+" "+emp1.salary)
+
+// 2) BY CREATING INSTANCE OF OBJECT
+
+var emp2=new Object();
+emp2.id=2;
+emp2.name="Amit Sahani";
+emp2.salary=50000;
+
+print(emp2.id+" "+emp2.name+" "+emp2.salary)
