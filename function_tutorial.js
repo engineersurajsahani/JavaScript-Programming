8db490230a8dd4b1971181c1e1536a43ee72baf8
Suraj Sahani <93707688+surajsahani9321@users.noreply.github.com>
2024-01-08 03:09:27 +0530

    08/01/2024

---
+++ function_tutorial.js
@@ -0,0 +1,36 @@
+// Function without parameter and without return statement
+
+function hello(){
+    print("Hello\n");
+}
+
+// Function with parameter and without return statement
+
+function add(a,b){
+    print("Addition of a and b is :- "+(a+b)+"\n");
+}
+
+// Function without parameter and with return statement
+
+function data(){
+    return 10;
+}
+
+// Function with parameter and with return statement
+
+function sum(a,b){
+    return a+b;
+}
+
+hello();
+
+add(5,6);
+
+a=data();
+print("Value of a is :- "+a);
+
+result=sum(7,8);
+print("\nResult :- "+result);
+
+
+
