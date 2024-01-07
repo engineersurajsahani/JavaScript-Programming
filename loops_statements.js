389014aeaffe0b5d4d6aacb4d46c11b95212d157
Suraj Sahani <93707688+surajsahani9321@users.noreply.github.com>
2024-01-08 03:00:24 +0530

    08/01/2023

---
+++ loops_statements.js
@@ -0,0 +1,21 @@
+print("***** For Loop *****");
+
+for(let i=1;i<11;i++){
+    print("2 * "+i+" = ",2*i);
+}
+
+print("***** While Loop *****");
+
+let i=1;
+while(i<11){
+    print("2 * "+i+" = ",2*i);
+    i++;
+}
+
+print("***** Do While Loop *****");
+
+i=1;
+do{
+    print("2 * "+i+" = ",2*i);
+    i++;
+}while(i<11);
