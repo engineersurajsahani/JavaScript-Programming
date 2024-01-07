a5d28cceeca91f2800877519c4c698163d5762a2
Suraj Sahani <93707688+surajsahani9321@users.noreply.github.com>
2024-01-08 02:55:07 +0530

    08/01/2023

---
+++ conditional_statements.js
@@ -0,0 +1,58 @@
+var days=1;
+
+if(days==1){
+    print("Monday");
+}
+else if(days==2){
+    print("Tuesday");
+}
+else if(days==3){
+    print("Wednesday");
+}
+else if(days==4){
+    print("Thursday");
+}
+else if(days==5){
+    print("Friday");
+}
+else if(days==6){
+    print("Saturday");
+}
+else if(days==7){
+    print("Sunday");
+}
+else{
+    print("Not Known");
+}
+
+switch(days){
+    case 1:
+        print("Monday");
+        break;
+    
+    case 2:
+        print("Tuesday");
+        break;
+        
+    case 3:
+        print("Wednesday");
+        break;
+        
+    case 4:
+        print("Thursday");
+        
+    case 5:
+        print("Friday");
+        break;
+        
+    case 6:
+        print("Saturday");
+        break;
+        
+    case 7:
+        print("Sunday");
+        break;
+        
+    default:
+        print("Not Known");
+}
